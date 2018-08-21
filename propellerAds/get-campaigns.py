517815fa1c89672589483a154cb7dfad776bf7a3
Fede Defaveri <f.g.defaveri@gmail.com>
2018-08-22 01:44:52 +0200

    Uses the new propellerAds api to get list of active campaigns

--- propellerAds/get-campaigns.py
+++ propellerAds/get-campaigns.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 
 ####
 # AUTHOR: Federico G. De Faveri
@@ -23,35 +23,49 @@ key = fincreds.readline()
 key = key.rstrip()
 
 # creating base url for our request
-base_api_url = "http://report.propellerads.com"
+base_api_url = "https://ssp-api.propellerads.com/v5/adv"
 
 # setting params
-action = 'getCampaigns'
-date_range = 'last_7_days'
-stat_columns = ['show','click','convers','convrate','cpm','ctr','profit']
-group_by = 'campaign_id'
+password = key.split(" ")[0]
+username = key.split(" ")[1]
 
 # setting payload
-payload = { 'action': action, 'key': key , 'date_range': date_range , 'stat_columns': stat_columns , 'group_by': group_by}
+payload = { 'username': username, 'password': password }
 
 # building the url
-url = base_api_url
+url = base_api_url + '/login'
 print("Sending GET request to " + url)
 print("")
 
 # making the api request
-r = requests.get(url, params=payload)
+r = requests.post(url, params=payload)
 
 # working with answer
 status = r.status_code
 
 print("STATUS: " + str(status)) 
 if status != 200:
-	print("Error")
+	print("Error!!!! <<<<<<<<")
 
+#receive the login api token
+login_api_token = r.json()["api_token"]
 
-pp.pprint(r.json())
+#setting variables
+url2 = base_api_url + '/campaigns'
+bearer_token = "Bearer " + login_api_token
+header2 = { "Authorization": bearer_token }
+payload2 = { "status[]": "6", "is_archived": "0"}
 
+# make the get all campaigns request
+r2 = requests.get(url2, headers=header2, params=payload2)
+
+#get the r2 status
+status2 = r2.status_code
+
+print ('R2 STATUS ----')
+print status2
+
+pp.pprint(r2.json())
 
 
 
